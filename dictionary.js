d4f090758e6ff36aea7d79bdf52e421f6ce36fcd
Vladimir <gorban469@gmail.com>
2022-03-21 19:37:24 +0200

    Delete commented code

--- dictionary.js
+++ dictionary.js
@@ -5,20 +5,17 @@ let search = document.getElementById('searchWord')
 function addTheWord () {
     dictionary += input.value
     input.value = ''
-    //sentence.innerHTML = ''
     sentence.innerHTML = '<div class="alert alert-info" role="alert">\n' +
         '  The word was added to the dictionary!\n' +
         '</div>'
 }
 function searchTheWord () {
     if (dictionary.indexOf(search.value) >= 0) {
-        //sentence.innerHTML = '<p></p>'
         sentence.innerHTML = '<div class="alert alert-success" role="alert">\n' +
             '  The word was found in the dictionary!\n' +
             '</div>'
         search.value = ''
     } else {
-        //sentence.innerHTML = '<p></p>'
         sentence.innerHTML = '<div class="alert alert-warning" role="alert">\n' +
             '  The word was not found in the dictionary, but you can add it using the box above!\n' +
             '</div>'
